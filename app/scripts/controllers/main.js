0bde778746b6e669afd24528d7bbde4c931a6fee
Wei <wrhuang@iii.org.tw>
2015-02-15 13:32:52 +0800

    init project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shoait01App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shoait01App
+ */
+angular.module('shoait01App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
