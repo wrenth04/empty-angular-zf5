dcf224ab45a6d7733551e94f533314e38e19ecce
Wei <wrhuang@iii.org.tw>
2015-02-15 14:02:18 +0800

    add foundation

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -19,6 +19,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
+      'bower_components/modernizr/modernizr.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
@@ -26,6 +28,10 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/fastclick/lib/fastclick.js',
+      'bower_components/jquery.cookie/jquery.cookie.js',
+      'bower_components/jquery-placeholder/jquery.placeholder.js',
+      'bower_components/foundation/js/foundation.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
